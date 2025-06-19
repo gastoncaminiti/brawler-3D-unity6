2e538e4d8db60e7609ea894c30669ed1e609f57c
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-06-19 10:40:00 -0300

    Estado Atacar

---
+++ Brawler U6/Assets/Scripts/EstadoAtacar.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EstadoAtacar : Estado
+{
+    [SerializeField]
+    private Animator m_Animator;
+    
+    public override void Iniciar()
+    {
+        AnimatorStateInfo stateInfo = m_Animator.GetCurrentAnimatorStateInfo(0);
+
+        if (stateInfo.IsName("Atk_K_1") && stateInfo.normalizedTime < 1f)
+            return;
+
+        m_Animator.SetTrigger("Atk_K_1"); 
+    }
+
+    public override void Actualizar(float deltaTime)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Finalizar()
+    {
+        throw new System.NotImplementedException();
+    }
+}
