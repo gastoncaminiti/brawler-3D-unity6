ff26cdc99118b1bf9b953699e6103d3b5428c2eb
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-06-19 14:01:27 -0300

    Actualizar Mover Estado

--- Brawler U6/Assets/Scripts/MoverEstado.cs
+++ Brawler U6/Assets/Scripts/MoverEstado.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class MoverEstado: Estado
 {
+    private const string triggerName = "Mover";
     public MoverEstado(MaquinaEstado maquinaEstado) : base(maquinaEstado)
     {
     }
@@ -9,11 +10,17 @@ public class MoverEstado: Estado
     public override void Iniciar()
     {
         Debug.Log("INICIAR ESTADO MOVER");
+        maquinaEstado.Animador.SetTrigger(triggerName); 
     }
 
     public override void Actualizar(float deltaTime)
     {
-        Debug.Log("ACTUALIZAR MOVIMIETNO");
+        Vector2 input = maquinaEstado.ObtenerInputMovimiento();
+        
+        Vector3 direccion = new Vector3(input.x, 0f, input.y);
+        
+        maquinaEstado.Controller.Move(direccion * maquinaEstado.VelocidadMovimiento * deltaTime);
+
     }
 
     public override void Finalizar()
