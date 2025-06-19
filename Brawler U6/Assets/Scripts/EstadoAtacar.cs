e6954c8749bdec409e1f06ada770a3147202700e
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-06-19 11:03:59 -0300

    Metodos EstadoAtacar

--- Brawler U6/Assets/Scripts/EstadoAtacar.cs
+++ Brawler U6/Assets/Scripts/EstadoAtacar.cs
@@ -1,33 +1,23 @@
 using UnityEngine;
-
 public class EstadoAtacar : Estado
 {
-    [SerializeField]
-    private Animator m_Animator;
-    
-    
     public EstadoAtacar(MaquinaEstado maquinaEstado) : base(maquinaEstado)
     {
     }
 
     public override void Iniciar()
     {
-        AnimatorStateInfo stateInfo = m_Animator.GetCurrentAnimatorStateInfo(0);
-
-        if (stateInfo.IsName("Atk_K_1") && stateInfo.normalizedTime < 1f)
-            return;
-
-        m_Animator.SetTrigger("Atk_K_1"); 
+        maquinaEstado.Animador.SetTrigger("Atk_K_1"); 
     }
     
     public override void Actualizar(float deltaTime)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("MIENTRAS SE ATACA");
     }
 
     public override void Finalizar()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("AL TERMINAR EL ATAQUE");
     }
 
 }
